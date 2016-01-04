a723d8db9abb77948f2cf31ec1bd646a3bff67c4
Taylor Crane <taylorkcrane@gmail.com>
2016-01-03 22:05:05 -0500

    forget what I did

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	before_save { self.email = self.email.downcase }
+	before_save { email.downcase! }
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :email, presence: true, length: { maximum: 255 },
 					format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
