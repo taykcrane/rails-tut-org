010f5a8e282749ff728aa8aea501b20babf67fa2
Taylor Crane <taylorkcrane@gmail.com>
2016-01-10 17:08:22 -0500

    built backend for remember me checkbox

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,15 +3,39 @@ module SessionsHelper
 		session[:user_id] = user.id
 	end
 
+	#remembers a user in a persistent session
+	def remember(user)
+		user.remember
+		cookies.permanent.signed[:user_id] = user.id
+		cookies.permanent[:remember_token] = user.remember_token
+	end
+
+	#sets @current_user by first seeing if there is an existing session. If not, but there is a cookie, then it logs in the user (by setting a session) first.
+	#also returns a valid user object
 	def current_user
-		@current_user ||= User.find_by(id: session[:user_id])
+		if session[:user_id]
+			@current_user ||= User.find_by(id: session[:user_id])
+		elsif cookies.signed[:user_id]
+			user = User.find_by(id: cookies.signed[:user_id])
+			if user && user.authenticated?(cookies.signed[:remember_token])
+				log_in user
+				@current_user = user
+			end
+		end
 	end
 
 	def logged_in?
 		!current_user.nil?
 	end
 
+	def forget(user)
+		user.forget
+		cookies.delete(:user_id)
+		cookies.delete(:remember_token)
+	end
+
 	def log_out
+		forget(current_user)
 		session.delete(:user_id)
 		@current_user = nil
 	end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,34 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }
 
+	#this is the token stored in the user's cookie. paired with the remember_digest stored in the DB
+	attr_accessor :remember_token
+
 	#returns the hash digest of a given string (for test database purposes)
 	def User.digest(string)
 		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                               		  BCrypt::Engine.cost
     	BCrypt::Password.create(string, cost: cost)
+	end
+
+	#returns a random token for remembering users
+	def User.new_token
+		SecureRandom.urlsafe_base64
+	end
+
+	#when called, stores the digest of :remember_token into the database
+	def remember
+		self.remember_token = User.new_token
+		update_attribute(:remember_token, User.digest(remember_token))
+	end
+
+	#returns true if the given raw token matches the digest in the database
+	def authenticated?(remember_token)
+		return false if remember_digest.nil?
+		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	end
+
+	def forget
+		update_attribute(:remember_digest, nil)
 	end
 end
