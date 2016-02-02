ad8a85c44b4091f141be34332b1bcae7bb88ed89
Taylor Crane <taylorkcrane@gmail.com>
2016-02-02 18:23:01 -0500

    continuing to work on microposts

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:create, :new, :edit, :update]
+  resources :microposts, only: [:create, :destroy]
 end
