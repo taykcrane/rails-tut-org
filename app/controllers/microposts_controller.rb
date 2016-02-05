aa75a61fb1542ecf8632d7d07ffcc8fea142422e
Taylor Crane <taylorkcrane@gmail.com>
2016-02-04 22:40:33 -0500

    users can create and destroy microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
 	before_action :logged_in_user, only: [:create, :destroy]
+	before_action :correct_user, only: [:destroy]
 
 	def create
 		@micropost = current_user.microposts.new(micropost_params)
@@ -7,16 +8,23 @@ class MicropostsController < ApplicationController
 			flash[:success] = "Micropost created!"
 			redirect_to root_path
 		else
-			render "static_page/home"
+			render "static_pages/home"
 		end
 	end
 
 	def destroy
-		
+		@micropost.destroy
+		flash[:success] = "Micropost deleted"
+		redirect_to request.referrer || root_path
 	end
 
 	private
 		def micropost_params
 			params.require(:micropost).permit(:content)
+		end
+
+		def correct_user
+			@micropost = current_user.microposts.find_by(id: params[:id])
+			redirect_to root_path if @micropost.nil?
 		end
 end
