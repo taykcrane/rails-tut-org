484927ebc380924ab6b258ed5489c92b10db24d9
Taylor Crane <taylorkcrane@gmail.com>
2016-02-13 17:16:29 -0500

    added working follow and unfollow buttons

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,5 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:create, :new, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
